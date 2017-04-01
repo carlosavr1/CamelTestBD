6179befba8601d02645a79e9149dee7e5c24ef87
Carlos Andres Vallejo Rengifo <carlvarn@suramericana.com.co>
2017-03-31 21:49:48 -0500

    Se crea ruta que lee una BD Oracle del esquema HR

---
+++ src/main/java/entity/Region.java
@@ -0,0 +1,24 @@
+package entity;
+
+public class Region {
+	private int regionId;
+	private String regionName;
+	
+	public int getRegionId() {
+		return regionId;
+	}
+	public void setRegionId(int regionId) {
+		this.regionId = regionId;
+	}
+	public String getRegionName() {
+		return regionName;
+	}
+	public void setRegionName(String regionName) {
+		this.regionName = regionName;
+	}
+	
+	@Override
+	public String toString(){
+		return "[Region = regionId:"+this.getRegionId()+", regionName:"+this.getRegionName()+"]";
+	}
+}
